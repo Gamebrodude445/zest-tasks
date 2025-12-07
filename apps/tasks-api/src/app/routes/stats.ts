b8e2492a3d9cd98964c8aa171211f708b526cf8b
Gamebrodude445 <sagib2003@gmail.com>
2025-12-07 16:58:32 +0200

    add swagger to stats

--- apps/tasks-api/src/app/routes/stats.ts
+++ apps/tasks-api/src/app/routes/stats.ts
@@ -1,7 +1,30 @@
 import { FastifyInstance } from 'fastify';
 
 export default async (fastify: FastifyInstance) => {
-  fastify.get('/statistics', (request, reply) => {
-    reply.send(fastify.tasks.getStatistics());
-  });
+  fastify.get(
+    '/statistics',
+    {
+      schema: {
+        description: 'Get task queue statistics',
+        tags: ['statistics'],
+        response: {
+          200: {
+            type: 'object',
+            properties: {
+              lifetimeTaskCounter: { type: 'number' },
+              numberOfTaskTries: { type: 'number' },
+              successToFailureRatio: { type: 'number' },
+              averageProcessingTime: { type: 'number' },
+              currentQueueLength: { type: 'number' },
+              idleWorkers: { type: 'number' },
+              hotWorkers: { type: 'number' },
+            },
+          },
+        },
+      },
+    },
+    (_request, reply) => {
+      reply.send(fastify.tasks.getStatistics());
+    }
+  );
 };
